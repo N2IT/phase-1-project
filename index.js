4c1110a3a60b98a8b0df16da5c50955e7e44c40c
Tony Eder <tonyeder11@gmail.com>
2023-11-14 11:08:00 -0800

    update to index.js with event listners and fetch to return data on 'click'

---
+++ index.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //event listeners
+    //Categories
+    document.getElementById('creatures').addEventListener('click', getCreatures)
+    document.getElementById('equipment').addEventListener('click', getEquipment)
+    document.getElementById('materials').addEventListener('click', getMaterials)
+    document.getElementById('monsters').addEventListener('click', getMonsters)
+    document.getElementById('treasures').addEventListener('click', getTreasures)
+
+    //SearchBar
+    document.getElementById('search').addEventListener('input', () => console.log('hello'))
+
+    //FETCH compendium API
+    function getCompendium(){
+        fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/all')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
+    }
+    getCompendium()
+    //FETCH
+    function getCreatures(){
+        fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/creatures')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
+    function getEquipment(){
+        fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/equipment')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
+    function getMaterials(){
+        fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/materials')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
+    function getMonsters(){
+        fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
+    function getTreasures(){
+        fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/treasure')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+})
