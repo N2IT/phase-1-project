eaeedbc3b718c774aed6a0063d50e85ee854758d
Tony Eder <tonyeder11@gmail.com>
2023-11-14 14:33:27 -0800

    work on processInputString function to render searched elements to DOM

--- index.js
+++ index.js
@@ -8,32 +8,50 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('treasures').addEventListener('click', getTreasures)
 
     //SearchBar
-    let search = document.getElementById('search')
-    search.addEventListener('input', (e) => {
-        let searchValue = e.target.value
-        processInputString(searchValue)
-    })
+    let form = document.querySelector('#compendium-form')
+    form.addEventListener('submit', handleSubmit)
 
-    //findItem via Search
-    function processInputString(event){
-        console.log(event)
+    function handleSubmit(e) {
+        e.preventDefault()
+        let searchValue = {
+            name: e.target.search.value
+        }
+        processInputString(searchValue)
+        form.reset()
+        // debugger
+    }
+
+    function processInputString(value) {
+        fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/all`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.github.v3+json',
+            }
+        })
+            .then(res => res.json())
+            .then(searchName => {
+                for (let key in searchName) {
+                    searchName[key].name.forEach((e) => renderAll(e))
+                }
+            })
+            debugger
+            .catch(error => console.error(error))
     }
 
     //FETCH compendium API
-    function getCompendium(){
+    function getCompendium() {
         fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/all')
-        .then(res => res.json())
-        .then(allItems => {
-            for(let key in allItems){
-                allItems[key].forEach((e) => renderAll(e))
-            }   
-        })
-        .catch(error => console.error(error))
-        debugger
+            .then(res => res.json())
+            .then(allItems => {
+                for (let key in allItems) {
+                    allItems[key].forEach((e) => renderAll(e))
+                }
+            })
+            .catch(error => console.error(error))
     }
-    
+
     //renderAll
-    function renderAll(item){
+    function renderAll(item) {
         let div = document.getElementById('items')
         let card = document.createElement('card')
         let img = document.createElement('img')
@@ -49,44 +67,44 @@ document.addEventListener('DOMContentLoaded', () => {
     //handle
 
     //FETCH
-    function getCreatures(){
+    function getCreatures() {
         fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/creatures')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
     }
 
-    function getEquipment(){
+    function getEquipment() {
         fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/equipment')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
     }
 
-    function getMaterials(){
+    function getMaterials() {
         fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/materials')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
     }
 
-    function getMonsters(){
+    function getMonsters() {
         fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
     }
 
-    function getTreasures(){
+    function getTreasures() {
         fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/treasure')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
     }
 
     //Initial Render
     //Get Data and render to DOM
-    function initialize(){
+    function initialize() {
         getCompendium()
     }
     initialize()
